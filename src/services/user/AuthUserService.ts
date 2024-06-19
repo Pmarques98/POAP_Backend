07b40dc67cdf700f45b668e9ce886be4d92df58f
Patrick Marques de Barros Costa <97074587+Pmarques98@users.noreply.github.com>
2024-06-19 15:33:08 -0300

    Initial commit

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -9,7 +9,7 @@ interface AuthRequest{
 
 class AuthUserService{
     async execute({email,password}: AuthRequest){
-        //Verificar se o email existe no banco de dados
+        //Verificar se o email existe
         const user = await prismaClient.user.findFirst({
             where:{
                 email:email
